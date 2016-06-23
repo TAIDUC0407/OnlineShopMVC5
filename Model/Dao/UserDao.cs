c7e773fc3482c0f375fa88e98dfa7de702e69e0a
TAI DUC <TAI DUC>
2016-06-23 22:52:15 +0700

    Bài 22: Tích hợp CK Finder để quản lý thư viện ảnh online vào ứng dụng ASP.NET MVC

--- Model/Dao/UserDao.cs
+++ Model/Dao/UserDao.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Model.EF;
+using PagedList;
+using PagedList.Mvc;
 
 namespace Model.Dao
 {
@@ -16,6 +18,30 @@ namespace Model.Dao
             db= new OnlineShopDbContext();
         }
 
+        public bool update(User entity)
+        {
+            try
+            {
+                var user = db.User.Find(entity.ID);
+                user.Name = entity.Name;
+                if (!string.IsNullOrEmpty(entity.Password))
+                {
+                    user.Password = entity.Password;
+                }
+                user.Address = entity.Address;
+                user.Email = entity.Email;
+                user.ModifiedBy = entity.ModifiedBy;
+                user.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //logging
+                return false;
+            }
+        }
+
         public long Insert(User entity)
         {
             db.User.Add(entity);
@@ -23,9 +49,27 @@ namespace Model.Dao
             return entity.ID;
         }
 
+        public IEnumerable<User> ListAllPaging(string searchString, int page, int pagesize)
+        {
+            IQueryable<User> model = db.User;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model =
+                    model.Where(x => x.UserName.Contains(searchString) || x.Name.Contains(searchString));
+            }
+
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pagesize);
+        }
+
         public User GetById(string userName)
         {
             return db.User.SingleOrDefault(x=>x.UserName==userName);
+        }
+
+        public User Viewdetail(int id)
+        {
+            return db.User.Find(id);
         }
 
         public int Login(string userName, string passWord)
@@ -49,6 +93,22 @@ namespace Model.Dao
                         return -2;
 
                 }
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var user = db.User.Find(id);
+                db.User.Remove(user);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
             }
         }
     }
